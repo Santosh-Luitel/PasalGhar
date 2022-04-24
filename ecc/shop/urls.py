5100e60aa06566b985906b4d2cd8407b2213d29d
Santosh-Luitel <santoshluitel785@gmail.com>
2022-04-24 12:49:43 +0530

    updated

---
+++ ecc/shop/urls.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from django.urls import path, include
+from . import views
+
+app_name = "main"
+
+urlpatterns = [
+    path('', views.index, name="ShopHome"),
+    path('about/', views.about, name="AboutUS"),
+    path('contact/', views.contact, name="ContactUs"),
+    path('tracker/', views.tracker, name="TrackingStatus"),
+    path('search/', views.search, name="Search"),
+    path('products/<int:myid>', views.productView, name="ProductView"),
+    path('checkout/', views.checkout, name="Checkout"),
+    path('signup', views.handleSignUp, name="handleSignUp"),
+    path('login', views.handeLogin, name="handleLogin"),
+    path('logout', views.handelLogout, name="handleLogout"),
+
+
+]
